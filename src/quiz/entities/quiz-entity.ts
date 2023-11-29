26b59698c4260cda348009dc9342fa3c1930b232
Matheus Motta <matheusdomingues423@gmail.com>
2023-11-29 10:37:48 -0300

    feat [quiz]: quiz entity

--- src/quiz/entities/quiz-entity.ts
+++ src/quiz/entities/quiz-entity.ts
@@ -1,22 +1,58 @@
-import { Controller, Get, Param } from '@nestjs/common';
-import { FindQuizRequestParams } from '../dtos/find-quiz-request-body';
-import { z } from 'zod';
+import {
+  Controller,
+  Post,
+  Body,
+  NotFoundException,
+  Get,
+  Param,
+} from '@nestjs/common';
 import { QuizRepository } from '../repositories/quiz-repository';
+import { CreateQuizRequestBody } from '../dtos/create-quiz-request-body';
+import axios from 'axios';
+import { z } from 'zod';
+import { FindQuizRequestParams } from '../dtos/find-quiz-request-params';
+import { addQuestionToQuizRequestBody } from '../dtos/add-to-quiz-request-body';
 
 @Controller('quiz')
 export class QuizController {
   constructor(private quizRepository: QuizRepository) {}
 
+  @Post('create')
+  async createQuiz(@Body() createQuizDto: CreateQuizRequestBody) {
+    const bodySchema = z.object({
+      name: z.string(),
+      description: z.string(),
+    });
+
+    const { name, description } = bodySchema.parse(createQuizDto);
+
+    let quiz = await this.quizRepository.create(name, description);
+
+    return { quiz };
+  }
+
   @Get(':id')
-  async findQuiz(@Param() findQuizDto: FindQuizRequestParams) {
+  async getQuiz(@Param() findQuizDto: FindQuizRequestParams) {
     const paramsSchema = z.object({
       id: z.string().uuid(),
     });
 
     const { id } = paramsSchema.parse(findQuizDto);
 
-    let quiz = await this.quizRepository.find(id);
+    const quiz = await this.quizRepository.find(id);
 
     return { quiz };
+  }
+
+  @Post('add/:amount')
+  async addQuestionToQuiz(@Param() addToQuizDto: addQuestionToQuizRequestBody) {
+    const paramsSchema = z.object({
+      amount: z.number(),
+      category: z.number(),
+      type: z.string(),
+      difficulty: z.string(),
+    });
+
+    const question = paramsSchema.parse(addToQuizDto);
   }
 }
