4aadab528c4a97664c7281d2d903166d0754c936
Matheus Motta <matheusdomingues423@gmail.com>
2023-11-29 10:38:12 -0300

    feat [quiz]: set quiz repositories

--- src/quiz/repositories/prisma/prisma-quiz-repository.ts
+++ src/quiz/repositories/prisma/prisma-quiz-repository.ts
@@ -1,11 +1,24 @@
 import { PrismaService } from 'src/services/prisma.services';
 import { QuizRepository } from '../quiz-repository';
-import { Quiz } from 'src/quiz/dtos/quiz-attributes';
+import { Question, Quiz } from 'src/quiz/dtos/quiz-attributes';
 import { Injectable } from '@nestjs/common';
 
 @Injectable()
 export class PrismaQuizRepository implements QuizRepository {
   constructor(private prisma: PrismaService) {}
+
+  addQuestion(question: Question): Promise<Question> {
+    throw new Error('Method not implemented.');
+  }
+
+  async create(name: string, description: string): Promise<Quiz> {
+    return await this.prisma.quiz.create({
+      data: {
+        name: name,
+        description: description,
+      },
+    });
+  }
 
   async find(id: string): Promise<Quiz> {
     return await this.prisma.quiz.findUnique({
--- src/quiz/repositories/quiz-repository.ts
+++ src/quiz/repositories/quiz-repository.ts
@@ -1,5 +1,9 @@
-import { Quiz } from '../dtos/quiz-attributes';
+import { Question, Quiz } from '../dtos/quiz-attributes';
 
 export abstract class QuizRepository {
   abstract find(id: string): Promise<Quiz>;
+
+  abstract create(name: string, description: string): Promise<Quiz>;
+
+  abstract addQuestion(question: Question): Promise<Question>;
 }
