21084cac1e806ae545e30397148d46933d7b6947
Matheus Motta <matheusdomingues423@gmail.com>
2023-12-02 19:52:27 -0300

    feat: prisma new repository method

--- src/quiz/repositories/prisma/prisma-quiz-repository.ts
+++ src/quiz/repositories/prisma/prisma-quiz-repository.ts
@@ -1,14 +1,50 @@
 import { PrismaService } from 'src/services/prisma.services';
 import { QuizRepository } from '../quiz-repository';
 import { Question, Quiz } from 'src/quiz/dtos/quiz-attributes';
-import { Injectable } from '@nestjs/common';
+import { ConflictException, Injectable } from '@nestjs/common';
 
 @Injectable()
 export class PrismaQuizRepository implements QuizRepository {
   constructor(private prisma: PrismaService) {}
 
-  addQuestion(question: Question): Promise<Question> {
-    throw new Error('Method not implemented.');
+  async addQuestion(id: string, question: Question): Promise<Question> {
+    const existingQuestion = await this.prisma.question.findUnique({
+      where: {
+        amount: question.amount,
+      },
+    });
+
+    if (existingQuestion) {
+      throw new ConflictException('this question is already in your quiz.');
+    }
+
+    return await this.prisma.question.create({
+      data: {
+        amount: question.amount,
+        category: question.category,
+        difficulty: question.difficulty,
+        type: question.type,
+        quiz: {
+          connect: {
+            id: id,
+          },
+        },
+      },
+    });
+  }
+
+  async list(): Promise<Quiz[]> {
+    const quizzes = await this.prisma.quiz.findMany({
+      orderBy: {
+        created_at: 'desc',
+      },
+
+      include: {
+        questions: true,
+      },
+    });
+
+    return quizzes;
   }
 
   async create(name: string, description: string): Promise<Quiz> {
@@ -24,6 +60,10 @@ export class PrismaQuizRepository implements QuizRepository {
     return await this.prisma.quiz.findUnique({
       where: {
         id: id,
+      },
+
+      include: {
+        questions: true,
       },
     });
   }
